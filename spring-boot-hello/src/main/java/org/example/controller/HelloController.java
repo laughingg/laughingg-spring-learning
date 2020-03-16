0979e9b881113516a4693b088ebcf109bc86cb98
laughing <zhuohong_xiao@163.com>
2020-03-16 20:19:48 +0800

    添加了运行结果图片

--- spring-boot-hello/src/main/java/org/example/controller/HelloController.java
+++ spring-boot-hello/src/main/java/org/example/controller/HelloController.java
@@ -1,8 +1,13 @@
 package org.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @RestController == @Controller + @ResponseBody
+ */
 @RestController
 public class HelloController {
 
